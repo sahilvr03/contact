ea6920908d53eaf7c44275bd2ceef6eab6a30b28
vijay rajesh <ewe111.vijay@gmail.com>
2026-05-13 10:45:37 +0500

    updated
    updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://crop2x.vercel.app",
+      "https://crop2x.com",
       "http://localhost:3000",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
